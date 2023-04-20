5d6d1ac84b1d90f53189800c4fedf5b9de496368
Jonas Werme <jonas.werme@teliacompany.com>
2023-04-20 13:53:42 +0200

    File tools: Add is_directory_writable

--- src/pywrapid/utils/file_tools.py
+++ src/pywrapid/utils/file_tools.py
@@ -65,3 +65,19 @@ def is_directory_readable(path: str) -> bool:
     return False
 
 
+def is_directory_writable(path: str) -> bool:
+    """Checks a directory in a given path to make sure it:
+        - exists
+        - is a directory
+        - is writable
+
+    Args:
+        path (str): Path to configuration file.
+
+    Returns:
+        bool: True/False.
+    """
+    if os.path.exists(path) and os.path.isdir(path) and os.access(path, os.W_OK):
+        return True
+
+    return False
