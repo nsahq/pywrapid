61aba37d715af6e26095905801b775b9e3c69f02
Jonas Werme <jonas.werme@teliacompany.com>
2023-04-28 12:51:26 +0200

    Tests: Add more utils tests

--- tests/test_pywrapid_utils_dict_tools.py
+++ tests/test_pywrapid_utils_dict_tools.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Pywrapid dict tools tests"""
 
-import os
-
 import pytest
 
 import pywrapid.utils.dict_tools as module_0
 
 # pylint: disable=redefined-outer-name, protected-access
+
 
 def test_utils_dict_tools_dict_merge_0():
     """Test dict_merge"""
@@ -15,6 +14,31 @@ def test_utils_dict_tools_dict_merge_0():
     data = {"b": {"c": 4, "e": 5}, "f": 6}
     expected = {"a": 1, "b": {"c": 4, "d": 3, "e": 5}, "f": 6}
     assert module_0.dict_merge(base, data) == expected
+
+
+def test_utils_dict_tools_dict_merge_1():
+    """Test dict_merge, merge None types if there is no base value"""
+    base = {"a": 1, "b": {"c": 2, "d": 3}}
+    data = {"b": {"c": 4, "e": None}, "f": 6}
+    expected = {"a": 1, "b": {"c": 4, "d": 3, "e": None}, "f": 6}
+    assert module_0.dict_merge(base, data) == expected
+
+
+def test_utils_dict_tools_dict_merge_2():
+    """Test dict_merge, skip None types if there is base value"""
+    base = {"a": 1, "b": {"c": 2, "d": 3, "e": 5}}
+    data = {"b": {"c": 4, "e": None}, "f": 6}
+    expected = {"a": 1, "b": {"c": 4, "d": 3, "e": 5}, "f": 6}
+    assert module_0.dict_merge(base, data) == expected
+
+
+def test_utils_dict_tools_dict_merge_3():
+    """Test dict_merge, raise on conflict"""
+    base = {"a": 1, "b": {"c": 2, "d": 3}, "f": 5}
+    data = {"b": {"c": 4, "e": 5}, "f": 6}
+    expected = {"a": 1, "b": {"c": 4, "d": 3, "e": 5}, "f": 6}
+    with pytest.raises(ValueError):
+        module_0.dict_merge(base, data, raise_on_conflict=True)
 
 
 def test_utils_dict_tools_dict_keys_exist_0():
@@ -52,3 +76,48 @@ def test_utils_dict_tools_dict_keys_exist_4():
     expected_keys = ["a", "b", "e"]
     with pytest.raises(ValueError):
         module_0.dict_keys_exist(data, expected_keys, raise_on_fail=True)
+
+
+def test_utils_dict_tools_dict_keys_exist_5():
+    """Test dict_keys_exist to raise on missing expected_keys"""
+    data = {"a": 1, "b": {"c": 2, "d": 3}, "e": []}
+    expected_keys = []
+    with pytest.raises(ValueError):
+        module_0.dict_keys_exist(data, expected_keys, raise_on_fail=True)
+
+
+def test_utils_dict_tools_dict_keys_exist_6():
+    """Test dict_keys_exist to raise on missing data/data missing expected_keys"""
+    data = {}
+    expected_keys = ["a", "b", "e"]
+    with pytest.raises(ValueError):
+        module_0.dict_keys_exist(data, expected_keys, raise_on_fail=True)
+
+
+def test_utils_dict_tools_dict_keys_exist_7():
+    """Test dict_keys_exist to raise on partial matches"""
+    data = {"a": 1, "b": {"c": 2, "d": 3}, "e": []}
+    expected_keys = ["a", "b", "e"]
+    with pytest.raises(ValueError):
+        module_0.dict_keys_exist(data, expected_keys, raise_on_fail=True)
+
+
+def test_utils_dict_tools_dict_keys_exist_8():
+    """Test dict_keys_exist partial matches without raise"""
+    data = {"a": 1, "b": {"c": 2, "d": 3}, "e": []}
+    expected_keys = ["a", "b", "e"]
+    assert module_0.dict_keys_exist(data, expected_keys, raise_on_fail=False) is False
+
+
+def test_utils_dict_tools_dict_keys_exist_9():
+    """Test dict_keys_exist missing data/data missing expected_keys without raise"""
+    data = {}
+    expected_keys = ["a", "b", "e"]
+    assert module_0.dict_keys_exist(data, expected_keys, raise_on_fail=False) is False
+
+
+def test_utils_dict_tools_dict_keys_exist_10():
+    """Test dict_keys_exist missing expected_keys without raise"""
+    data = {"a": 1, "b": {"c": 2, "d": 3}, "e": []}
+    expected_keys = []
+    assert module_0.dict_keys_exist(data, expected_keys, raise_on_fail=False) is False
